4e9bd392ce2f81b64ea93403cb9aaff60ec0f756
bM001-o <bhadreswarmundary@gmail.com>
2026-08-19 20:19:35 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size());
+        dp[0] = nums[0];
+
+        for(int i = 1;i < nums.size();i++){
+            if(i == 1){
+                dp[i] = max(nums[i],dp[0]);
+            }
+            else{
+                dp[i] = max(nums[i] + dp[i - 2],dp[i - 1]);
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+};
