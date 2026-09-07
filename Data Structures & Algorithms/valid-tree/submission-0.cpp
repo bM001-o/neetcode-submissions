35b733e44b4d44710ac70ef96f4bf243a7dbc801
bM001-o <bhadreswarmundary@gmail.com>
2026-09-07 20:20:47 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(int node,int parent,vector<vector<int>>& adj,unordered_set<int>& visit){
+        if(visit.count(node)){
+            return false;
+        }
+        visit.insert(node);
+        
+        for(int neighbor : adj[node]){
+            if(neighbor == parent){
+                continue;
+            }
+            if(!dfs(neighbor,node,adj,visit)){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size() > n - 1){
+            return false;
+        }
+        vector<vector<int>>adj(n);
+
+        for(const auto& edge : edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        unordered_set<int> visit;
+        if(!dfs(0,-1,adj,visit)){
+            return false;
+        }
+        return visit.size() == n;
+    }
+};
