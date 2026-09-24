f95177fd266ca4e76bf01ae50b0856e6b6202927
bM001-o <bhadreswarmundary@gmail.com>
2026-09-24 12:04:57 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+
+        def dfs(r,c,i):
+            if i == len(word):
+                return True
+            
+            if (r >= ROWS or r < 0 or c >= COLS or c < 0 or word[i] != board[r][c] or board[r][c] == '#'):
+                return False
+
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            val =  dfs(r + 1,c,i + 1) or dfs(r - 1,c,i + 1) or dfs(r,c + 1,i + 1) or dfs(r,c - 1,i + 1)
+            board[r][c] = temp
+
+            return val
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r,c,0):
+                    return True
+        
+        return False
